143d2edc405bd44ba8c09a6d41e241a53f24fd10
unknown <keitaku85@gmail.com>
2025-07-24 13:11:59 -0700

    test

---
+++ OneDrive/デスクトップ/css fun/fizzbuzz.js
@@ -0,0 +1,15 @@
+for (i = 0; i <=10; i++)
+{
+    if (i % 5 == 0){
+            console.log(i, "fizzbuzz")
+
+        }
+    else if (i % 2 == 0){
+
+            console.log(i, "fizz")
+         }else{
+         console.log(i,"buzz")
+    }
+       
+    
+}
---
+++ OneDrive/デスクトップ/css fun/te.js
@@ -0,0 +1,20 @@
+var ggg =8
+
+let ff = 9
+
+const tt = 10
+const teet = [1,2,3,4]
+
+console.log(tt)
+const tty = parseFloat(0.1+0.2)
+teet.map((key,index))=>{
+    console.log('Index')
+}
+const tty = (5 +5)*6
+console.log(tty)
+for(let i =0;i<tt;i++){
+    console.log(i)
+    if(i>7{
+        console.log()
+    })
+}
